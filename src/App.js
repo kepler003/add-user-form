ac0c30d2cc8ccd9e78c7afe0d5c0f39da965bfe5
Bartek <b.pyzocha@gmail.com>
2022-02-21 23:23:43 +0100

    Add basic html structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <p>Hello world</p>;
+  return (
+    <main>
+      <form>
+        <div>
+          <label>Username</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Age (years)</label>
+          <input type="number" />
+        </div>
+        <div>
+          <button>Add user</button>
+        </div>
+      </form>
+      <ul>
+        <li>John (25 years old)</li>
+        <li>Jane (24 years old)</li>
+      </ul>
+    </main>
+  );
 }
 
 export default App;
