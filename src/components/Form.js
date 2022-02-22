88c0bbf4b25203adee881bf5e733c56ba2f64354
Bartek <b.pyzocha@gmail.com>
2022-02-22 20:57:22 +0100

    Add submit btn styling

---
+++ src/components/Form.js
@@ -0,0 +1,24 @@
+import Card from './Card';
+import Button from './Button';
+import Input from './Input';
+import styles from './Form.module.css';
+
+const Form = () => {
+  return (
+    <Card tag='form'>
+      <div className={`${styles['form__input-box']} ${styles['form__input-box--no-margin']}`}>
+        <label className={styles['form__label']}>Username</label>
+        <Input type='text' />
+      </div>
+      <div className={styles['form__input-box']}>
+        <label className={styles['form__label']}>Age (years)</label>
+        <Input type='number' />
+      </div>
+      <div className={`${styles['form__input-box']} ${styles['form__input-box--buttons']}`}>
+        <Button type="submit" className={styles['form__btn--submit']}>Add user</Button>
+      </div>
+    </Card>
+  );
+};
+
+export default Form;
