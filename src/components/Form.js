e2d0141fba1c8438e6655b19811730ff70c5e678
Bartek <b.pyzocha@gmail.com>
2022-02-22 21:30:22 +0100

    Add onChangeHandler fn

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,49 @@
+import { useState } from 'react';
 import Card from './Card';
 import Button from './Button';
 import Input from './Input';
 import styles from './Form.module.css';
 
 const Form = () => {
+  const [data, setData] = useState({
+    name: '',
+    age: '',
+  });
+
+  const onChangeHandler = (e) => {
+    const prop = e.target.getAttribute('name');
+    setData((prevData) => ({ ...prevData, [prop]: e.target.value }));
+  };
+
   return (
     <Card>
       <form>
-        <div className={`${styles['form__input-box']} ${styles['form__input-box--no-margin']}`}>
+        <div
+          className={`${styles['form__input-box']} ${styles['form__input-box--no-margin']}`}
+        >
           <label className={styles['form__label']}>Username</label>
-          <Input type='text' />
+          <Input
+            onChange={onChangeHandler}
+            type='text'
+            name='name'
+            value={data.name}
+          />
         </div>
         <div className={styles['form__input-box']}>
           <label className={styles['form__label']}>Age (years)</label>
-          <Input type='number' />
+          <Input
+            onChange={onChangeHandler}
+            type='number'
+            name='age'
+            value={data.age}
+          />
         </div>
-        <div className={`${styles['form__input-box']} ${styles['form__input-box--buttons']}`}>
-          <Button type="submit" className={styles['form__btn--submit']}>Add user</Button>
+        <div
+          className={`${styles['form__input-box']} ${styles['form__input-box--buttons']}`}
+        >
+          <Button type='submit' className={styles['form__btn--submit']}>
+            Add user
+          </Button>
         </div>
       </form>
     </Card>
