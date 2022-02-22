5d81078d8603454168fcc74e2e8996d6ea187296
Bartek <b.pyzocha@gmail.com>
2022-02-22 21:18:23 +0100

    Add user rendering

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,16 @@
 import Card from './Card';
 import styles from './List.module.css';
 
-const List = () => {
+const List = ({ users }) => {
   return (
     <Card className={styles.list__card}>
       <ul className={styles.list}>
-        <li>John (25 years old)</li>
-        <li>Jane (24 years old)</li>
+        {!users.length && <p>No users found.</p>}
+        {!!users.length &&
+          users.map(({ name, age }) => {
+            const phrase = age === 1 ? 'year' : 'years';
+            return <li>{`${name} (${age} ${phrase} old)`}</li>;
+          })}
       </ul>
     </Card>
   );
