9796705dae4a268e05c582d49f2900f9fee71f26
Bartek <b.pyzocha@gmail.com>
2022-02-22 21:22:20 +0100

    Add key prop

--- src/components/List.js
+++ src/components/List.js
@@ -2,15 +2,17 @@ import Card from './Card';
 import styles from './List.module.css';
 
 const List = ({ users }) => {
+  const LiElems = users.map(({ name, age, id }) => {
+    const phrase = age === 1 ? 'year' : 'years';
+    const msg = `${name} (${age} ${phrase} old)`;
+    return <li key={id}>{msg}</li>;
+  });
+
   return (
     <Card className={styles.list__card}>
       <ul className={styles.list}>
         {!users.length && <p>No users found.</p>}
-        {!!users.length &&
-          users.map(({ name, age }) => {
-            const phrase = age === 1 ? 'year' : 'years';
-            return <li>{`${name} (${age} ${phrase} old)`}</li>;
-          })}
+        {!!users.length && LiElems}
       </ul>
     </Card>
   );
