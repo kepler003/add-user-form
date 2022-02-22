80be9ede934d3c210c6c66b134354d928c87c8e1
Bartek <b.pyzocha@gmail.com>
2022-02-22 21:06:29 +0100

    Remove switch statement

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,13 @@
 import styles from './Card.module.css';
 
-const Card = (props) => {
-  const { tag, children } = props;
-
-  const childProps = {
+const Card = ({ children, ...props }) => {
+  props = {
     ...props,
     children: undefined,
-    tag: undefined,
     className: `${styles.card} ${props.className ?? ''}`,
   };
 
-  switch (tag) {
-    case 'form':
-      return <form {...childProps}>{children}</form>;
-    case 'ul':
-      return <ul {...childProps}>{children}</ul>;
-    default:
-      return <div {...childProps}>{children}</div>;
-  }
+  return <div {...props}>{children}</div>;
 };
 
 export default Card;
