a885a72091c69ae92d74cd8f4dad1797d97bc871
Bartek <b.pyzocha@gmail.com>
2022-02-22 21:06:41 +0100

    Separate Card from form tag

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,18 +5,20 @@ import styles from './Form.module.css';
 
 const Form = () => {
   return (
-    <Card tag='form'>
-      <div className={`${styles['form__input-box']} ${styles['form__input-box--no-margin']}`}>
-        <label className={styles['form__label']}>Username</label>
-        <Input type='text' />
-      </div>
-      <div className={styles['form__input-box']}>
-        <label className={styles['form__label']}>Age (years)</label>
-        <Input type='number' />
-      </div>
-      <div className={`${styles['form__input-box']} ${styles['form__input-box--buttons']}`}>
-        <Button type="submit" className={styles['form__btn--submit']}>Add user</Button>
-      </div>
+    <Card>
+      <form>
+        <div className={`${styles['form__input-box']} ${styles['form__input-box--no-margin']}`}>
+          <label className={styles['form__label']}>Username</label>
+          <Input type='text' />
+        </div>
+        <div className={styles['form__input-box']}>
+          <label className={styles['form__label']}>Age (years)</label>
+          <Input type='number' />
+        </div>
+        <div className={`${styles['form__input-box']} ${styles['form__input-box--buttons']}`}>
+          <Button type="submit" className={styles['form__btn--submit']}>Add user</Button>
+        </div>
+      </form>
     </Card>
   );
 };
